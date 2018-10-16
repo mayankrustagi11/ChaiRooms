51ec81070108f337ad3e1ea88a7fb3ecaa80ce70
rails -v <mayankrustagi11@gmail.com>
2018-10-17 00:18:29 +0530

    ChaiRooms

--- js/main.js
+++ js/main.js
@@ -1,51 +1,27 @@
 $(document).ready(function () {
 
     // Menu Function
-    $('.nav-button').click(function () {
+    $('.nav-button').click(() => {
         $('.nav-button, .side-nav, .nav-header, .nav-options').toggleClass('nav-open');
         return false;
     });
 
-    // Food Menu function
-    $('.nav-link.the-menu').click(function () {
-        $('.nav-button, .side-nav, .nav-header, .nav-options').removeClass('nav-open');
-        $('.menu-container').addClass('displayed');
-        return false;
-    });
-
-    // closing the menu-overlay
-    $('.close, .menu-container').click(function () {
-        $('.menu-container').removeClass('displayed');
-        return false;
-    });
-    // preventing clicks on the menu closing it for now because there is nothing inside yet.
-    $('.tab.menu-tab').click(function () {
-        return false;
-    });
-
-    $(function () {
-        $.scrollify({
-            section: ".section",
-            easing: "easeOutExpo",
-            scrollSpeed: 2000,
-            offset: 0,
-            scrollbars: false,
-            target: "html,body",
-            standardScrollElements: true,
-            setHeights: true,
-            overflowScroll: true,
-            updateHash: false,
-            touchScroll: true,
-        });
-    });
-
-    // Window scroll function
-    $(window).scroll(function () {
-
-        if ($(window).scrollTop() > 600) {
-            $('.nav-button, .search-button').addClass('black');
-        } else {
-            $('.nav-button, .search-button').removeClass('black');
+    $('#fullpage').fullpage({
+        css3: true,
+        autoScrolling: true,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        fitToSection: true,
+        scrollOverflow: true,
+        scrollBar: false,
+        keyboardScrolling: true,
+        normalScrollElements: '.side-nav',
+        onLeave: function (origin, destination, direction) {
+            if (destination.index == 0) {
+                $('.nav-button').removeClass('black');
+            } else {
+                $('.nav-button').addClass('black');
+            }
         }
     });
 });
